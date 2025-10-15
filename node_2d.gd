77fe931e59649353978399d96e66ac06dc554451
Edralzar <edralzar@daedra.eu>
2025-10-15 19:29:05 +0200

    Implement beat challenges and tween (N)PCs

--- node_2d.gd
+++ node_2d.gd
@@ -5,31 +5,52 @@ extends Node2D
 @onready var guide: Label = $GuidanceLabel
 @onready var judge: Label = $JudgeLabel
 
-# The beats method signature is:
-#
-#   func beats(beat_count: float, repeating := true, start_beat := 0.0) -> Signal
-#
-# We'll use this below to create repeating signals that emit every `beat_count`
-# beats, starting on `start_beat`, and non-repeating signals that emit when we reach
-# `start_beat`.
+@export var notes : Array[String] = ["C", "E", "G", "B"]
+@onready var synth: Sampler = $Sampler
+
+var pTween: Tween
+var oTween: Tween
+var tweenBeat: float
+var challenge: int = 0
+var stop := false
 
 func _ready() -> void:
-	_play_some_music()
+	var beatMs := r.beat_length
+	r.beats(1).connect(func(count): 
+		guide.text = "Go" if (count >= 4) else "Wait!"
+		judge.text = "*"
+		
+		if (count == 7):
+			challenge += 1
+			print("Challenge %d" % challenge)
+			if (challenge == 2):
+				stop = true
+				oTween.stop()
+				pTween.stop()
+				$Player/Sprite2D.scale = Vector2.ONE
+				$Other/Sprite2D.scale = Vector2.ONE
+				await get_tree().create_timer(beatMs / 2).timeout
+				r.audio_stream_player.stop()
+		elif stop:
+			judge.text = ""
+			guide.text = "The End"
+	)
+	r.beats(1, true, 4).connect(func(count): _judge())
+	
+	pTween = get_tree().create_tween().set_loops()
+	pTween.tween_property($Player/Sprite2D, "scale", Vector2(0.8 , 0.8), beatMs * 0.75)
+	pTween.tween_property($Player/Sprite2D, "scale", Vector2(1.0, 1.0), beatMs * 0.25)
+	oTween = get_tree().create_tween().set_loops()
+	oTween.tween_property($Other/Sprite2D, "scale", Vector2(0.9 , 0.9), beatMs * 0.75)
+	oTween.tween_property($Other/Sprite2D, "scale", Vector2(1.0, 1.0), beatMs * 0.25)
+	
 	r.audio_stream_player.play()
 
-# Play music and emit lots of signals
-func _play_some_music():
-	# Print on beat 0, 4, 8, 12...
-	r.beats(1).connect(func(count): l.text = "Hello from beat %d!" % (count * 4 / 4))
-#
-	## Print on beat 2, 5, 8, 11...
-	#r.beats(3, true, 2).connect(func(count): print("Hello from beat %d!" % 2+(count * 3)))
-#
-	## Print anytime beat 8.5 is reached.  The first param is ignored when the second is `false`..
-	#r.beats(0, false, 8.5).connect(func(_i): print("Hello from beat eight and a half!"))
-#
-	#r.audio_stream_player.play()  # Start signaling
-	#r.audio_stream_player.seek(1.5)  # pausing/stopping/seeking all supported
-#
-	## Stop playback on beat 20
-	#r.beats(0, false, 20).connect(func(_i): r.audio_stream_player.stop())
+
+func _judge():
+	if (randf() > 0.75):
+		synth.play_note("B", 4)
+		judge.text = "WRONG"
+	else:
+		synth.play_note("B", 4)
+		judge.text = "GREAT"
