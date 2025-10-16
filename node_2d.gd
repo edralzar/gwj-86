1f0543908a4d3637c0d0486d76ebaf3c8c660629
Edralzar <edralzar@daedra.eu>
2025-10-16 11:29:28 +0200

    Refactor HUD, add judging note fit

--- node_2d.gd
+++ node_2d.gd
@@ -2,28 +2,40 @@ extends Node2D
 
 # Set r.bpm and r.audio_stream_player in inspector
 @onready var r: RhythmNotifier = $RhythmNotifier
-@onready var guide: Label = $GuidanceLabel
+@onready var guide: Label = $GuideLabel
+@onready var progress: Label = $ProgressLabel
 @onready var judge: Label = $JudgeLabel
+@onready var synth: Sampler = $Sampler
 
 @export var notes : Array[String] = ["C", "E", "G", "B"]
-@onready var synth: Sampler = $Sampler
+@export var responses : Array[String] = [ "D", "D", "D", "E"]
+@export var max_attempts : int = 2
 
 var pTween: Tween
 var oTween: Tween
 var tweenBeat: float
-var challenge: int = 0
+var attempt: int = 0
+var playerTurn: bool = false
 var stop := false
 
 func _ready() -> void:
 	var beatMs := r.beat_length
-	r.beats(1).connect(func(count): 
-		guide.text = "Go" if (count >= 4) else "Wait!"
-		judge.text = "*"
+	r.beats(1).connect(func(count):
+		playerTurn = count >= 4
+		progress.text = "ATTEMPT %d / %d" % [attempt+1, max_attempts]
+		if (not playerTurn):
+			judge.text = ""
+			guide.text = "Listen..." if (attempt == 0) else "Listen again..."
+		else:
+			guide.text = "GO !"
+		
+		if (!playerTurn && count < notes.size()):
+			synth.play_note(notes[count], 4)
 		
 		if (count == 7):
-			challenge += 1
-			print("Challenge %d" % challenge)
-			if (challenge == 2):
+			attempt += 1
+			print("Attempt %d" % attempt)
+			if (attempt == max_attempts):
 				stop = true
 				oTween.stop()
 				pTween.stop()
@@ -31,11 +43,14 @@ func _ready() -> void:
 				$Other/Sprite2D.scale = Vector2.ONE
 				await get_tree().create_timer(beatMs / 2).timeout
 				r.audio_stream_player.stop()
+				judge.text = ""
+				guide.text = "The End"
 		elif stop:
+			#Extraneous beat, ensuring cleanup
 			judge.text = ""
 			guide.text = "The End"
 	)
-	r.beats(1, true, 4).connect(func(count): _judge())
+	r.beats(1, true, 4).connect(_judge)
 	
 	pTween = get_tree().create_tween().set_loops()
 	pTween.tween_property($Player/Sprite2D, "scale", Vector2(0.8 , 0.8), beatMs * 0.75)
@@ -47,10 +62,13 @@ func _ready() -> void:
 	r.audio_stream_player.play()
 
 
-func _judge():
-	if (randf() > 0.75):
-		synth.play_note("B", 4)
-		judge.text = "WRONG"
-	else:
-		synth.play_note("B", 4)
+func _judge(count: int):
+	count = count % 4 #sanitize
+	#simulate user input for now
+	var playerNote = "D" if (randf() > 0.75) else "E"
+	synth.play_note(playerNote, 4)
+	
+	if (playerNote == responses[count]):
 		judge.text = "GREAT"
+	else:
+		judge.text = "WRONG"
